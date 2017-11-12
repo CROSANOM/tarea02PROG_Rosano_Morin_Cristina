f24c4be2d1862c479d3c3c1bca186b23459d1635
CROSANOM <crosanom@hotmail.com>
2017-11-12 12:44:05 +0100

    Update numeroConstruido.java

--- Tarea02Pro1718/src/numeroContruido/numeroConstruido.java
+++ Tarea02Pro1718/src/numeroContruido/numeroConstruido.java
@@ -3,9 +3,9 @@
  */
 package numeroContruido;
 
-/*Ejercicio 2. Escribe un programa que dado un n˙mero positivo realice el desplazamiento del mismo una posiciÛn hacia la derecha.
-El desplazamiento hacia la derecha consiste en coger la ˙ltima cifra de la derecha del n˙mero y ponerla a la izquierda. Por ejemplo: 2476 -> 6247, 123 -> 312, 987 -> 798....
-El programa debe asegurarse de que se introduce un n˙mero positivo.*/
+/*Ejercicio 2. Escribe un programa que dado un n√∫mero positivo realice el desplazamiento del mismo una posici√≥n hacia la derecha.
+El desplazamiento hacia la derecha consiste en coger la √∫ltima cifra de la derecha del n√∫mero y ponerla a la izquierda. Por ejemplo: 2476 -> 6247, 123 -> 312, 987 -> 798....
+El programa debe asegurarse de que se introduce un n√∫mero positivo.*/
 
 /**
  * @author crosanom
@@ -30,6 +30,7 @@ public class numeroConstruido {
 
 		System.out.println(restoNumero);// imprime 310
 		System.out.print("el numero que hemos construido es  " + ultimoNumero + restoNumero);
+		System.out.println("Hola es una prueba");
 
 		// TODO Auto-generated method stub
 
